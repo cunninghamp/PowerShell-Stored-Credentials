b71e18460bd1f203eed71730539f94c600a2d793
Paul Cunningham <paul@locklan.com.au>
2018-10-22 09:09:45 +1000

    Update Functions-PSStoredCredentials.ps1

--- Functions-PSStoredCredentials.ps1
+++ Functions-PSStoredCredentials.ps1
@@ -17,16 +17,10 @@ Written by: Paul Cunningham
 
 Find me on:
 
-* My Blog:	http://paulcunningham.me
+* My Blog:	https://paulcunningham.me
 * Twitter:	https://twitter.com/paulcunningham
-* LinkedIn:	http://au.linkedin.com/in/cunninghamp/
+* LinkedIn:	https://au.linkedin.com/in/cunninghamp/
 * Github:	https://github.com/cunninghamp
-
-For more Office 365 tips, tricks and news
-check out Practical 365.
-
-* Website:	https://practical365.com
-* Twitter:	https://twitter.com/practical365
 #>
 
 
